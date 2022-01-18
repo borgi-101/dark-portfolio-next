8f3279c565c864cd99a741c0cf1e73ce7f0a4e5c
Franco Borgiani <contact@franborgiani.com>
2022-01-18 14:55:11 -0300

    [FIX] md screen size

--- pages/about/index.js
+++ pages/about/index.js
@@ -4,9 +4,9 @@ import ThemeContext from "../../context/ThemeContext";
 const About = () => {
     const { darkmode, setDarkmode } = useContext(ThemeContext);
     return (
-        <div className="w-full h-screen max-w-6xl px-8 mx-auto text-2xl text-center text-pink-100">
-            <section className="grid h-screen max-w-6xl px-6 mx-auto place-content-center">
-                <h1 className={`max-w-6xl text-4xl  ${darkmode ? "text-white" : "text-black"}`}>
+        <div className="w-full h-screen max-w-6xl mx-auto text-2xl text-center text-pink-100 md:px-8">
+            <section className="grid max-w-6xl px-6 mx-auto md:h-screen place-content-center">
+                <h1 className={`max-w-6xl md:text-4xl  ${darkmode ? "text-white" : "text-black"}`}>
                     I'm 18, I'm a tech student, and lover at linux, In my free time I drink cofee, and read about tech. I love run, and I try do it every day.
                 </h1>
             </section>
--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,9 @@ import ThemeContext from "../context/ThemeContext";
 const Home = () => {
     const { darkmode, setDarkmode } = useContext(ThemeContext)
     return (
-        <div className="w-full h-screen max-w-6xl px-8 mx-auto text-2xl text-center text-pink-100">
-            <section className="grid h-screen max-w-6xl px-6 mx-auto place-content-center">
-                <h1 className={`max-w-6xl text-4xl  ${darkmode ? "text-white" : "text-black"}`}>
+        <div className="w-full h-screen max-w-6xl mx-auto text-2xl text-center text-pink-100 md:px-8">
+            <section className="grid max-w-6xl px-6 mx-auto md:h-screen place-content-center">
+                <h1 className={`max-w-6xl md:text-4xl  ${darkmode ? "text-white" : "text-black"}`}>
                     Hi i'm Franco Borgiani, Fullstack Developer, and I love Frontend technologies, like NextJS & React.
                 </h1>
             </section>
